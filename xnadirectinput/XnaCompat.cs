d18746e668d5bfea7a6b871b47129953a4e137ee
Derek Chafin <infomaniac50@gmail.com>
2012-11-08 17:24:54 -0600

    Removed reference to Xna Framework and added XnaCompat.cs standin or easy compiling.

---
+++ xnadirectinput/XnaCompat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Xna.Framework
+{
+    public struct Vector2
+    {
+        public Vector2(float X, float Y) : this()
+        {
+            
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public static Vector2 Zero
+        {
+            get{
+                return new Vector2();
+            }            
+        }
+
+        public float X
+        {
+            get;
+            set;
+        }
+
+        public float Y
+        {
+            get;
+            set;
+        }
+    }
+}
+
+namespace Microsoft.Xna.Framework.Input
+{
+    public enum ButtonState
+    {
+        Pressed,
+        Released
+    }
+}
